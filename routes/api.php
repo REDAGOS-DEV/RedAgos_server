77631bc8e9e5332606abdb5baf525a85201dea29
Vizxyyy <rafaelababa9@gmail.com>
2026-07-10 04:20:56 +0800

    Added donor registration route

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,24 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DonorDashboardController;
+use App\Http\Controllers\DonorProfileController;
+use App\Http\Controllers\DonorRegistrationController;
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/donors/register', [DonorRegistrationController::class, 'register'])
+    ->middleware('throttle:5,1');
+Route::get('/donors/dashboard', [DonorDashboardController::class, 'show'])
+    ->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function (): void {
+    Route::get('/donors/profile', [DonorProfileController::class, 'show']);
+    Route::patch('/donors/profile', [DonorProfileController::class, 'update']);
+    Route::put('/donors/profile', [DonorProfileController::class, 'update']);
+    Route::post('/donors/password', [DonorProfileController::class, 'updatePassword']);
+    Route::patch('/donors/notification-preferences', [DonorProfileController::class, 'updateNotificationPreferences']);
+});
 
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+    return $request->user()?->load(['roles', 'donorProfile.bloodType']);
 })->middleware('auth:sanctum');
-
-
