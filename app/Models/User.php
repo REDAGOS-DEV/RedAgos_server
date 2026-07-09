afd1ae8af6ff54a14f6b2af1f304581eb2b7225b
Vizxyyy <rafaelababa9@gmail.com>
2026-07-10 04:21:27 +0800

    Added donor registration fillable fields:phone account_status activated_at

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,23 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-#[Fillable(['uuid', 'first_name', 'last_name', 'email', 'username', 'password'])]
+#[Fillable([
+    'uuid',
+    'first_name',
+    'last_name',
+    'email',
+    'phone',
+    'username',
+    'password',
+    'account_status',
+    'activated_at',
+])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
@@ -27,7 +39,18 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
+            'activated_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class)->withTimestamps();
+    }
+
+    public function donorProfile(): HasOne
+    {
+        return $this->hasOne(DonorProfile::class, 'donor_id');
     }
 }
